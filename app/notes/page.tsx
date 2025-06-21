0ae45cbaa2ef67ad4e7716dccc0603054f77cbc7
Daria <darak0751@gmail.com>
2025-06-21 23:08:06 +0300

    first commit

---
+++ app/notes/page.tsx
@@ -0,0 +1,12 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Note page',
+};
+
+const Notes = () => {
+    return <div>Notes</div>;
+  };
+  
+  export default Notes;
+  
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Home',
+};
+
+const Home = () => {
+  return <div>Welcome to Home</div>
+}
+
+export default Home
