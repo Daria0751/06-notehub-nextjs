71a93c28962a6060474fb5785e7a660289aceaf2
Daria <darak0751@gmail.com>
2025-06-27 23:38:47 +0300

    hw6

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,19 +1,10 @@
-import type { Metadata } from 'next';
 import NotesClient from './Notes.client';
 import { getNotes } from '@/lib/api';
 
-export const metadata: Metadata = {
-  title: 'Notes Page',
-};
-
-export default async function Page() {
+export default async function NotesPage() {
   const data = await getNotes();
 
-  return (
-    <section>
-      <NotesClient initialData={data} />
-    </section>
-  );
+  return <NotesClient initialData={data} />;
 }
 
 
