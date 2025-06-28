df2c9ef1cb5007fad621c61dd2cba28cbff5bf5d
Daria <darak0751@gmail.com>
2025-06-28 10:16:36 +0300

    hw6

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -2,9 +2,13 @@ import NotesClient from './Notes.client';
 import { getNotes } from '@/lib/api';
 
 export default async function NotesPage() {
-  const data = await getNotes();
-
-  return <NotesClient initialData={data} />;
+  try {
+    const data = await getNotes();
+    return <NotesClient initialData={data} />;
+  } catch (error) {
+    console.error('Failed to load notes at build time:', error);
+    return <p>Failed to load notes</p>;
+  }
 }
 
 
